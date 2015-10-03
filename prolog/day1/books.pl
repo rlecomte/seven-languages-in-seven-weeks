73a6f53b2dde76a43e9b8d026784273fc6b9d3de
Romain Lecomte <romain.lecomte01@gmail.com>
2015-10-03 16:24:04 +0200

    feat(finish day1)

---
+++ prolog/day1/books.pl
@@ -0,0 +1,6 @@
+
+book_author('A la recherche du temps perdu', 'Proust').
+book_author('Le rouge et le noir', 'Stendhal').
+book_author('La rabouilleuse', 'Balzac').
+book_author('La curée', 'Zola').
+
---
+++ prolog/day1/musicians.pl
@@ -0,0 +1,5 @@
+
+musicians_instru('Tom Morello', 'Guitare').
+musicians_instru('Santana', 'Guitare').
+musicians_instru('Armstrong', 'Trompette').
+
